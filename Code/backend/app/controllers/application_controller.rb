fd559f040297c9d205643ccbda712c1a3a2c08bb
Tercio de Melo <terciodemelo@gmail.com>
2017-01-14 10:56:51 -0300

    [Backend] Adds authentication verification helper
    Now controllers can use the helper method `authenticate_request!`

--- Code/backend/app/controllers/application_controller.rb
+++ Code/backend/app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+  
+  protected
+  def authenticate_request!
+    unless user_id_in_token?
+      return render_unauthorized
+    end
+
+    @current_user = User.find(auth_token[:user_id])
+  rescue JWT::VerificationError, JWT::DecodeError
+    render_unauthorized
+  end
+
+  private
+  def render_unauthorized
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
+
+  def http_token
+    @http_token ||= if request.headers['Authorization'].present?
+                      request.headers['Authorization'].split.last
+                    end
+  end
+
+  def auth_token
+    @auth_token ||= JsonWebToken.decode(http_token)
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
 end
