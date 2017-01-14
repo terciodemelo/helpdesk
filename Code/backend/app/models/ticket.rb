d067dd95e9ab081a5e33c42190da49e78ebe58a9
Tercio de Melo <terciodemelo@gmail.com>
2017-01-14 19:23:58 -0300

    [Backend] Implements Tickets validation through TDD

--- Code/backend/app/models/ticket.rb
+++ Code/backend/app/models/ticket.rb
@@ -1,3 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :author, :foreign_key => 'author_id', class_name: "User"
+
+  validates :title, :body, :status, :author, presence: true
+  validates :status, inclusion: { in: %w(OPEN CLOSED) }
 end
--- Code/backend/spec/models/ticket_spec.rb
+++ Code/backend/spec/models/ticket_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Model validity" do
+    it "is is invalid if has nil title" do
+      expect(build :invalid_nil_title_ticket).to_not be_valid(Ticket)
+    end
+
+    it "is is invalid if has empty title" do
+      expect(build :invalid_empty_title_ticket).to_not be_valid(Ticket)
+    end
+
+    it "is is invalid if has nil body" do
+      expect(build :invalid_nil_body_ticket).to_not be_valid(Ticket)
+    end
+
+    it "is is invalid if has empty body" do
+      expect(build :invalid_empty_body_ticket).to_not be_valid(Ticket)
+    end
+
+    it "is is invalid if status is neither 'OPEN' or 'CLOSED'" do
+      expect(build :invalid_wrong_status_ticket).to_not be_valid(Ticket)
+    end
+  end
 end
