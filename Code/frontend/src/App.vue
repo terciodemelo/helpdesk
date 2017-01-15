d50198ed630642564e7fe12f724a4a431de15f1a
Tercio de Melo <terciodemelo@gmail.com>
2017-01-15 15:31:18 -0300

    [Frontend] Adds a navigation bar to all pages

--- Code/frontend/src/App.vue
+++ Code/frontend/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <nav class="nav has-shadow">
+      <div class="nav-left">
+        <a class="nav-item is-tab" href="/">Home</a>
+        <a class="nav-item is-tab" href="/tickets">Tickets</a>
+      </div>
+      <div class="nav-right">
+        <a class="nav-item is-tab">Logout</a>
+      </div>
+    </nav>
+    <div id="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -8,12 +19,22 @@
 </script>
 
 <style>
+nav {
+  width: 100%;
+}
+
 #app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#content {
+  max-width: 50em;
 }
 </style>
