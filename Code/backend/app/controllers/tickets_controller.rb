2f184bff26a55395916d965f359531e5a6c599d5
Tercio de Melo <terciodemelo@gmail.com>
2017-01-14 23:35:08 -0300

    [Doc] Adds in-code documentation to TicketController

--- Code/backend/app/controllers/tickets_controller.rb
+++ Code/backend/app/controllers/tickets_controller.rb
@@ -3,6 +3,10 @@ class TicketsController < ApplicationController
   before_action :authenticate_request!
 
   # GET /tickets
+  # Responds with all the tickets that the currently logged in user
+  # has access to. If this user is a SupportAgent or an Admin, they
+  # have access to all tickets, but if it is a Customer, then it has
+  # access to their own tickets
   def index
     @tickets = case current_user
                when Customer then Ticket.where(author_id: current_user.id)
@@ -13,6 +17,9 @@ class TicketsController < ApplicationController
   end
 
   # GET /tickets/1
+  # Responds with the details for the requested ticket if the
+  # currently logged user is a SupportAgent or Admin, or they are
+  # the ticket's author
   def show
     case current_user
     when Customer
@@ -27,6 +34,8 @@ class TicketsController < ApplicationController
   end
 
   # POST /tickets
+  # Handles the creation of new tickets, but only Customers
+  # are allowed to create new Tickets
   def create
     case current_user
     when Customer
@@ -44,6 +53,11 @@ class TicketsController < ApplicationController
   end
 
   # PATCH/PUT /tickets/1
+  # Handles the update of existing tickets. The currently logged
+  # in user must be either the ticket's creator or a SupportAgent
+  # or an Admin
+  #
+  # @see can_update_ticket?
   def update
     if can_update_ticket? @ticket
       if @ticket.update(ticket_params)
@@ -57,6 +71,8 @@ class TicketsController < ApplicationController
   end
 
   # DELETE /tickets/1
+  # Handles the deletion of existing tickets, only Admins are
+  # allowed to do it
   def destroy
     case current_user
     when Admin
@@ -67,18 +83,23 @@ class TicketsController < ApplicationController
   end
 
   private
-    def can_update_ticket? ticket
-      not current_user.is_a? Customer or current_user.id == ticket.author_id
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ticket
-      @ticket = Ticket.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render :status => :not_found
-    end
+  # For a given Ticket instance, verifies if the currently logged in
+  # user is allowed to update the ticket
+  #
+  # @param [Ticket] ticket 
+  # @return [boolean] 
+  def can_update_ticket? ticket
+    not current_user.is_a? Customer or current_user.id == ticket.author_id
+  end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render :status => :not_found
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def ticket_params
-      params.require(:ticket).permit(:title, :body, :status, :author_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def ticket_params
+    params.require(:ticket).permit(:title, :body, :status, :author_id)
+  end
 end
