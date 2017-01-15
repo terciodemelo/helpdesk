09e1798cc4c2a4e83e92c1fb29da8d61b199bb62
Tercio de Melo <terciodemelo@gmail.com>
2017-01-15 09:47:53 -0300

    [Frontend] Configures initial project settings

--- Code/frontend/src/main.js
+++ Code/frontend/src/main.js
@@ -1,11 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App'
+import Hello from './components/Hello'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '*', component: Hello}
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  router: router,
+  render: h => h(App)
 })
