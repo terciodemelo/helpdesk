ef25c2d02566729cc90fd05d4c6d0c0c8514c236
Tercio de Melo <terciodemelo@gmail.com>
2017-01-16 13:08:35 -0300

    [DB Seed] Adds SupportAgent and Admin seeds

--- Code/backend/db/seeds.rb
+++ Code/backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SupportAgent.create(
+  name: 'Support Um',
+  email: 'support1@mailinator.com',
+  password: 'support1'
+)
+
+
+Admin.create(
+  name: 'Admin',
+  email: 'admin@mailinator.com',
+  password: 'admin'
+)
