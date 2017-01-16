6834edcc8e6af66a1d2741933929b1b816ec832b
Tercio de Melo <terciodemelo@gmail.com>
2017-01-16 12:59:35 -0300

    [Backend] Undoing :index set_ticket in TicketsController

--- Code/backend/app/controllers/tickets_controller.rb
+++ Code/backend/app/controllers/tickets_controller.rb
@@ -1,5 +1,5 @@
 class TicketsController < ApplicationController
-  before_action :set_ticket, only: [:index, :show, :update, :destroy]
+  before_action :set_ticket, only: [:show, :update, :destroy]
   before_action :authenticate_request!
 
   # GET /api/tickets
