11c71e73449e780bf75026a0ca962d2dbdea432b
Tercio de Melo <terciodemelo@gmail.com>
2017-01-17 00:59:59 -0300

    [Backend] Adds responses_count to ticket serializer

--- Code/backend/app/serializers/ticket_serializer.rb
+++ Code/backend/app/serializers/ticket_serializer.rb
@@ -1,9 +1,14 @@
 class TicketSerializer < ActiveModel::Serializer
   attributes Ticket.column_names
   attribute :author_name
+  attribute :responses_count
   has_many :ticket_responses
 
   def author_name
     object.author.name
+  end
+
+  def responses_count
+    object.ticket_responses.count
   end
 end
