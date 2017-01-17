d9d1145da62bb35984e81f64a7d43970dbb1c2d6
Tercio de Melo <terciodemelo@gmail.com>
2017-01-17 14:27:33 -0300

    [Backend] Respondes tickets count alongwith customer

--- Code/backend/app/models/customer.rb
+++ Code/backend/app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < User
-  has_many :tickets
+  has_many :tickets, foreign_key: :author_id
 
   def self.model_name
     User.model_name
--- Code/backend/app/serializers/user_serializer.rb
+++ Code/backend/app/serializers/user_serializer.rb
@@ -1,3 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes User.column_names.select { |c| c != "password" }
+  attribute :closed_tickets
+  attribute :open_tickets
+
+  def closed_tickets
+    object.tickets.where(status: 'CLOSED').count if object.is_a? Customer
+  end
+
+  def open_tickets
+    object.tickets.where(status: 'OPEN').count if object.is_a? Customer
+  end
 end
