55948dd0a460b2e74a393a975e8772070699ad9d
Tercio de Melo <terciodemelo@gmail.com>
2017-01-17 17:06:12 -0300

    [Backend] Removes responses from ticket serialization

--- Code/backend/app/serializers/ticket_serializer.rb
+++ Code/backend/app/serializers/ticket_serializer.rb
@@ -2,7 +2,6 @@ class TicketSerializer < ActiveModel::Serializer
   attributes Ticket.column_names
   attribute :author_name
   attribute :responses_count
-  has_many :ticket_responses
 
   def author_name
     object.author.name
