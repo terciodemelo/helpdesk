7d893500089c83f212070db4f61ff7682485b7a0
Tercio de Melo <terciodemelo@gmail.com>
2017-01-19 01:59:58 -0300

    [Frontend] NewTicket in-code documentation

--- Code/frontend/src/components/tickets-list-elements/NewTicket.vue
+++ Code/frontend/src/components/tickets-list-elements/NewTicket.vue
@@ -39,12 +39,30 @@ export default {
   name: 'new-ticket',
   data () {
     return {
+      /*
+       * Stores the ticket's title
+       */
       title: '',
+
+      /*
+       * Stores the ticket's body
+       */
       body: '',
+
+      /*
+       * boolean that indicates if the form should be visible or not, this
+       * value changes according to clicks in the 'New Ticket' button and
+       * the 'Cancel' button
+       */
       formVisible: false
     }
   },
   methods: {
+    /*
+     * Handles the form submission, when clicking the button 'Create Ticket'
+     * by emitting upwards the event 'new-ticket' alongwith the form data
+     * which shall be handled by the enclosing component
+     */
     submit () {
       let payload = {
         title: this.title,
@@ -57,9 +75,23 @@ export default {
       this.body = ''
       this.formVisible = false
     },
+
+    /*
+     * Simple toggles the 'formVisible' attribute, which then controls the
+     * form visibility
+     */
     toggleForm () {
       this.formVisible = !this.formVisible
     },
+
+    /*
+     * This method is called to define the visibility of the 'New Ticket'
+     * button which must be hidden unless the currently logged in user is a
+     * Customer.
+     *
+     * @return [String] CSS class name 'hidden' if the currently logged in
+     *                  user is not a customer
+     */
     visibility () {
       return AuthHelper.isCustomer() ? '' : 'hidden'
     }
