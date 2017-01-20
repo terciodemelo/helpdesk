19da90962dc0e832a718ed3b3691b206dca28cf7
Tercio de Melo <terciodemelo@gmail.com>
2017-01-20 02:44:34 -0300

    [Backend] Bugfix: setups cascade deletion

--- Code/backend/app/models/customer.rb
+++ Code/backend/app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < User
-  has_many :tickets, foreign_key: :author_id
+  has_many :tickets, foreign_key: :author_id, dependent: :destroy
 
   def self.model_name
     User.model_name
--- Code/backend/app/models/ticket.rb
+++ Code/backend/app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :author, :foreign_key => 'author_id', class_name: "User"
-  has_many :ticket_responses
+  has_many :ticket_responses, dependent: :destroy
 
   validates :title, :body, :status, :author, presence: true
   validates :status, inclusion: { in: %w(OPEN CLOSED) }
